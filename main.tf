87fc0fc75719fc7e1f189a100bc2f54cd61219d5
Manjunath Bhadrannavar <31785721+manjunathb4461@users.noreply.github.com>
2025-06-15 07:12:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,19 +6,8 @@ terraform {
   }
 }
 
-resource "spacelift_stack" "example-stack" {
-  name = var.new_stack_name
-
-  administrative    = true
-  autodeploy        = false
-  branch            = "master"
-  description       = "Shared production infrastructure (networking, k8s)"
-  repository        = "testing-spacelift"
-  terraform_version = "0.12.27"
-}
-
 resource "spacelift_webhook" "simple-webhook-4" {
-  stack_id = spacelift_stack.example-stack.id
+  stack_id = iac-sandbox-envs-lab
 
   endpoint = "https://example.com/example_webhook"
   secret   = "my_secret"
