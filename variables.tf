40c161ae4f93855396783c314eebf343016202fe
Manjunath Bhadrannavar <31785721+manjunathb4461@users.noreply.github.com>
2025-06-15 07:18:42 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,3 +4,9 @@ variable "new_stack_name" {
   type = string
   description = "Description for the stack to create."
 }
+
+variable "new_stack_id" {
+  type = string
+  default = "iac-sandbox-envs-lab"
+  description = "Description for the stack to create."
+}
