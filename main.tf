6c077970b62cc055b2abf0830bf6b83d1ff03951
Manjunath Bhadrannavar <31785721+manjunathb4461@users.noreply.github.com>
2025-06-15 07:20:15 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 resource "spacelift_webhook" "simple-webhook-4" {
-  stack_id = "iac-sandbox-envs-lab"
+  stack_id = var.new_stack_id
 
   endpoint = "https://example.com/example_webhook"
   secret   = "my_secret"
